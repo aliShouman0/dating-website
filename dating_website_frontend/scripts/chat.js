756a7d58362cf48805a4b6940e407e6ad69b1f4f
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-10-04 17:07:59 +0300

    Frontend: Linking Api 'messages' with JS to let  user can get all chat with other users 'load them on page' by clicking on their name in the chat page

--- dating_website_frontend/scripts/chat.js
+++ dating_website_frontend/scripts/chat.js
@@ -1,10 +1,81 @@
 const left_panel = document.getElementById("left_panel");
+const message_box = document.getElementById("message_box");
+const userInfo = JSON.parse(localStorage.getItem("user_info"));
 
+
+const removeSelected=()=>{
+  const selected = document.querySelectorAll(".selected");
+  selected.forEach(element => {
+    element.classList.remove("selected");
+    
+  });
+
+
+}
+
+
+const loadMessage = (data) => {
+  message_box.innerHTML = ""
+  if (data.status == "Success") {
+    data.data.forEach(element => {
+
+      if (element.id == userInfo.id) {
+        const d = new Date(element.date * 1000);
+        message_box.innerHTML += `
+        <div class="send">
+          <div class="date"><span>${d.toDateString()}</span></div>
+          <div class="text"><span>${element.text}</span></div>
+        </div>`;
+      } else {
+        const d = new Date(element.date * 1000);
+        message_box.innerHTML += `
+        <div class="received">
+          <div class="date"><span>${d.toDateString()}</span></div>
+          <div class="text"><span>${element.text}</span></div>
+        </div>
+        `;
+      }
+      message_box.innerHTML += `
+      <div class="new-message">
+      <input type="text" placeholder="Enter message" class="input">
+      <button class="send-message">Send!</button>
+      </div>`
+    });
+
+  }
+
+}
+
+const loadEvent = () => {
+  const user_receiver = document.querySelectorAll(".user_receiver");
+  user_receiver.forEach(element => {
+    element.addEventListener("click", async () => {
+      removeSelected()
+      element.classList.add("selected")
+      const url = `${dating_website.baseUrl}/messages/${element.getAttribute("data-value")}?token=${dating_website.token}`
+      const res = await dating_website.getAPI(url);
+      loadMessage(res.data)
+
+    });
+  });
+  const user_senders = document.querySelectorAll(".user_senders");
+  user_senders.forEach(element => {
+    element.addEventListener("click", async () => {
+      removeSelected()
+      element.classList.add("selected")
+      const url = `${dating_website.baseUrl}/messages/${element.getAttribute("data-value")}?token=${dating_website.token}`
+      const res = await dating_website.getAPI(url);
+      loadMessage(res.data)
+
+    });
+  });
+
+}
 
 const loadData = (data) => {
   data.senders.forEach(element => {
     left_panel.innerHTML += ` 
-    <div class="user  selected" data-value="${element.user_sender.id}">
+    <div class="user user_senders " data-value="${element.user_sender.id}">
      <div class="img">
        <img src="assets/client.png" alt="img">
      </div>
@@ -14,7 +85,7 @@ const loadData = (data) => {
   });
   data.receivers.forEach(element => {
       left_panel.innerHTML += ` 
-    <div class="user " data-value="${element.user_receiver.id}" >
+    <div class="user user_receiver" data-value="${element.user_receiver.id}" >
      <div class="img">
        <img src="assets/client.png" alt="img">
      </div>
